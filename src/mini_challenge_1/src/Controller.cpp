85ec99cc2e91dc19e20ef7e73fb2046a9a237342
PAOK-2001 <2001.paok@gmail.com>
2023-04-15 09:57:40 -0600

    Added path_generator node

---
+++ src/mini_challenge_1/src/Controller.cpp
@@ -0,0 +1,3 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <cmath>
---
+++ src/mini_challenge_1/src/PathGenerator.cpp
@@ -0,0 +1,38 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Path.h>
+
+using namespace std;
+
+int nodeRate = 100;
+vector<int> path;
+vector<int> prevPath;
+
+nav_msgs::Path generate_path(vector <int> path){
+    nav_msgs::Path generatedPath;
+    for(int coord = 0; coord < path.size() - 2; coord+=2){
+        geometry_msgs::PoseStamped currentCoord;
+        currentCoord.pose.position.x = path[coord];
+        currentCoord.pose.position.y = path[coord+1];
+        generatedPath.poses.push_back(currentCoord);
+    }
+    return generatedPath;
+}
+
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "path_generator");
+    ros::NodeHandle handler;
+    ros::Publisher pathPublisher = handler.advertise<nav_msgs::Path>("/path",10);
+    ros::Rate rate(nodeRate);
+    while (ros::ok()) {
+        ros::param::get("/desired_path", path);
+        if(path != prevPath){
+            pathPublisher.publish(generate_path(path));
+        }
+        prevPath = path;
+        rate.sleep();
+    }
+    return 0;
+
+}
