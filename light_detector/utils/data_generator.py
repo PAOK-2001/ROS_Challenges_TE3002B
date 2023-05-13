58728962ee5ccffc2536ec67496691f72c827b1a
PAOK-2001 <2001.paok@gmail.com>
2023-05-13 19:22:56 +0100

    Added test files

--- light_detector/utils/data_generator.py
+++ light_detector/utils/data_generator.py
@@ -2,17 +2,23 @@ import cv2 as cv
 import os
 
 delay = 150
+disp_width = 640
+disp_height = 480
+flip = 0
+cam_port =  'nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(disp_width)+', height='+str(disp_height)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 
 def save_frame(frame, path, count):
-    cv.imwrite(path+'frame%d.jpg'% count, frame)
+    cv.imwrite(path+'well_lit%d.jpg'% count, frame)
+
 
 if __name__ == "__main__":
-    count = 0
-    camera = cv.VideoCapture(1)  
+    count = 420
+    camera = cv.VideoCapture(cam_port)  
     while True: 
         ret, frame = camera.read()
-        save_frame(frame,"../data/raw/", count)
-        cv.imshow('frame', frame)
+        frame = cv.GaussianBlur(frame,(5,5),0)
+        save_frame(frame,"../data/raw/", count) 
+        '''cv.imshow('frame', frame)'''
         count += 1
         if cv.waitKey(delay) & 0xFF == ord('q'):
             break
